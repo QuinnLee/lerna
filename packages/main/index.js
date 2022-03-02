32093ab68629aee921b9ac3ca64515762e3a8512
Quinn Lee <leekeunhwi@gmail.com>
2022-03-01 19:56:03 -0500

    feat: foobar
    BREAKING CHANGE: The foo

--- packages/main/index.js
+++ packages/main/index.js
@@ -1,3 +1,5 @@
 import p1 from 'p1-lerna-semantic-release-test';
 import p2 from 'p2-lerna-semantic-release-test';
 import p3 from 'p3-lerna-semantic-release-test';
+import p3 from 'p3-lerna-semantic-release-test';
+import p3 from 'p3-lerna-semantic-release-test';
